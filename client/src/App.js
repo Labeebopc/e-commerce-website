c185177e30c9b459952e81a8fb0508e0bc631686
Labeebopc <Labeebopc313@gmail.com>
2023-02-18 19:04:34 +0530

    created landig-page and products listed

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/header/header';
+import Products from './components/products/products';
 
 function App() {
   return (
     <>
-      Hello
+    <BrowserRouter>
+    <Header/>
+    <Products/>
+    </BrowserRouter>    
     </>
   );
 }
